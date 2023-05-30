fbcd7b40927bf87258ff4de3b2cc4a8f5e9b626d
fallmo <ps.hackmaster@gmail.com>
2023-05-30 14:33:17 +0000

    fibonacci

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ app.get("/version", (req, res) => {
   res.send("App running version 1.0\n");
 });
 
+
+app.get("/fibonacci", async (req, res) => {
+  function fibonacci(num) {
+  if (num <= 1) return num;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+	}
+
+	const result = fibonacci(Math.ceil(Math.random(0,1)*100)));
+
+	
+	res.rend(`Result ${result}`);
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Application running on 0.0.0.0:${PORT}\n`));
